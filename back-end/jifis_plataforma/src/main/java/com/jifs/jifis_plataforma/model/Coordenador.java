2eaa8cf1390ce63d3862904472de71d468637890
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-08 16:06:26 -0300

    [Remove] importações não utilizadas para limpar o código

--- back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenador.java
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenador.java
@@ -2,12 +2,9 @@ package com.jifs.jifis_plataforma.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
