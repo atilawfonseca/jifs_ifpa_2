c870946e583bb9db11b4deac9303eb515c7b8174
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-08 13:01:10 -0300

    Validaçoes adicionadas nos atributos do model: [Coordenadores]

--- back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
@@ -1,11 +1,14 @@
 package com.jifs.jifis_plataforma.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "coordenadores")
@@ -15,9 +18,17 @@ public class Coordenadores {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "O nome de usuário é obrigatório")
+    @Size(min = 3, max = 50)
     private String nome;
+
+    @Email(message = "O e-mail deve ser válido")
+    @NotBlank(message = "O email é obrigatório")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    @Column(nullable = false, unique = true)
     private String telefone;
 
 }
