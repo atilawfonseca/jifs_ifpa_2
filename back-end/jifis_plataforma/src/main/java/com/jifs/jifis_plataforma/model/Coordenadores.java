e3298fb4c5071c228645fb98ace7c1e1b5131200
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-08 13:08:04 -0300

    Construtores e Metodos get and Setters adicionado no model: [Coordenadores]

--- back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
@@ -31,4 +31,49 @@ public class Coordenadores {
     @Column(nullable = false, unique = true)
     private String telefone;
 
+    public Coordenadores(){
+
+    }
+
+    public Coordenadores(Long id, String nome, String email, String telefone){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+
 }
