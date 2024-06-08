40cf937e8800cb99c26b04059b88534970093565
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-08 11:51:51 -0300

    Configuraçoes do back-end iniciada

---
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/JifisPlataformaApplication.java
@@ -0,0 +1,13 @@
+package com.jifs.jifis_plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JifisPlataformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JifisPlataformaApplication.class, args);
+	}
+
+}
