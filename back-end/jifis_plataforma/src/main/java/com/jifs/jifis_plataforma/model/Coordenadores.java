726a4706fef9b75f69c67a9837c3cea49ee74ceb
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-08 12:48:03 -0300

    Adicionada Dependencia de Validação no pom.xml

--- back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/model/Coordenadores.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "coordenadores")
@@ -14,6 +15,7 @@ public class Coordenadores {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
     private String email;
     private String telefone;
