428d6e2d6067b3376e9909b10a52c005bd12ba18
luizxdev <luizfelipe.ifpa.2022@gmail.com>
2024-06-12 23:13:14 -0300

    Readme.md adicionado

--- back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/controller/CoordenadorController.java
+++ back-end/jifis_plataforma/src/main/java/com/jifs/jifis_plataforma/controller/CoordenadorController.java
@@ -9,7 +9,6 @@ import com.jifs.jifis_plataforma.services.CoordenadoresService;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
